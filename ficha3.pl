38832682d67e3cc51f4a8d38bd6ce8372b22da6e
Unknown <paulojorgecome@gmail.com>
2019-02-25 00:01:11 +0000

    init

---
+++ ficha3.pl
@@ -0,0 +1,54 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 1) Construir a extensão do predicado «pertence» que verificase um elemento existe dentro de uma lista de elementos
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 2) Construir a extensão do predicado «comprimento» que calculao número de elementos existentes numa lista
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 3) Construir a extensão do predicado «diferentes» que calculaa quantidade de elementos diferentes existentes numa lista
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 4) Construir a extensão do predicado «apaga1» que apaga a primeira ocorrência de um elemento de uma lista
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 5) Construir a extensão do predicado «apagaT»que apagatodas as ocorrências de um dado elemento numa lista
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 6) Construir a extensão do predicado «adicionar»que insereum elementonuma lista, sem o repetir
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 7) Construir a extensão do predicado «concatenar», que resulta na concatenação dos elementos da lista L1 com os elementos da lista L2
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 8) Construir a extensão do predicado «inverter»que inverte a ordem dos elementos de uma lista
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 9) Construir a extensão do predicado «sublista» que determinase uma lista S é uma sublista deoutra listaL
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 10) Construir a extensão de um predicado capaz de encontrar todas as possibilidades de prova de um teorema.
